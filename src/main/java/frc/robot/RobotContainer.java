09c80d5976751697f5a29d10e991a130856dccd3
EsatTürsun <tursunesat@gmail.com>
2024-01-25 21:59:52 +0300

    PathPlanner Upgraded

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,7 +3,12 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import java.util.List;
+
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.GoalEndState;
 import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 
@@ -18,6 +23,7 @@ import edu.wpi.first.wpilibj.smartdashboard.FieldObject2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -28,6 +34,7 @@ import frc.robot.commands.autonomous.AutoBalance;
 import frc.robot.poseestimation.PoseEstimation;
 import frc.robot.subsystems.drivetrain.Drivetrain;
 import frc.robot.utils.AllianceUtils;
+import edu.wpi.first.math.util.Units;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -68,7 +75,14 @@ public class RobotContainer {
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
      autoChooser = AutoBuilder.buildAutoChooser("New Auto");
-  SmartDashboard.putData("Auto Chooser", autoChooser);
+  SmartDashboard.putData("Auto Mod", autoChooser);
+    
+     NamedCommands.registerCommand("marker1", Commands.print("Passed marker 1"));
+    NamedCommands.registerCommand("marker2", Commands.print("Passed marker 2"));
+    NamedCommands.registerCommand("print hello", Commands.print("hello"));
+
+
+
   drivetrain.setDefaultCommand(driveCommand);
 
   if (autoBalanceStartingPosition.getPoses().isEmpty()) {
@@ -107,17 +121,66 @@ public class RobotContainer {
 
       new JoystickButton(joystick1, 3)
               .whileTrue(autoBalanceCommand);
+
+   SmartDashboard.putData("Example Auto", new PathPlannerAuto("new Auto"));
+
+   SmartDashboard.putData("Pathfind to 1 Pos", AutoBuilder.pathfindToPose(
+    new Pose2d(2.70, 2.01, Rotation2d.fromDegrees(0)), 
+    new PathConstraints(
+      3.0, 3.0, 
+      Units.degreesToRadians(540), Units.degreesToRadians(720)
+    ), 
+    0, 
+    2.67
+  ));
+
+  SmartDashboard.putData("Pathfind to 2 Pos", AutoBuilder.pathfindToPose(
+    new Pose2d(2.15, 3.0, Rotation2d.fromDegrees(180)), 
+    new PathConstraints(
+      4.0, 4.0, 
+      Units.degreesToRadians(540), Units.degreesToRadians(720)
+    ), 
+    0, 
+    3.35
+  ));
+
+  // Add a button to SmartDashboard that will create and follow an on-the-fly path
+    // This example will simply move the robot 2m in the +X field direction
+    SmartDashboard.putData("On-the-fly path", Commands.runOnce(() -> {
+      Pose2d currentPose = poseEstimation.getEstimatedPose();
       
+      // The rotation component in these poses represents the direction of travel
+      Pose2d startPos = new Pose2d(currentPose.getTranslation(), new Rotation2d());
+      Pose2d endPos = new Pose2d(currentPose.getTranslation().plus(new Translation2d(2.0, 0.0)), new Rotation2d());
+
+      List<Translation2d> bezierPoints = PathPlannerPath.bezierFromPoses(startPos, endPos);
+      PathPlannerPath path = new PathPlannerPath(
+        bezierPoints, 
+        new PathConstraints(
+          3.0, 3.0, 
+          Units.degreesToRadians(360), Units.degreesToRadians(540)
+        ),  
+        new GoalEndState(0.0, currentPose.getRotation())
+      );
+
+      // Prevent this path from being flipped on the red alliance, since the given positions are already correct
+      path.preventFlipping = true;
+
+      AutoBuilder.followPath(path).schedule();
+    }));
+  
   }
+   
 
   public Command getAutonomousCommand() {
     Pose2d startingPose = startingPosition.getPose();
-       PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path1");
+    //PathPlannerPath path = PathPlannerPath.fromPathFile("Example Path 1");
     return new SequentialCommandGroup(
     new InstantCommand(() -> poseEstimation.resetPose(startingPose)),
     new InstantCommand(() -> poseEstimation.resetPose(startingPose)),
-     autoChooser.getSelected().andThen(AutoBuilder.pathfindThenFollowPath(path, new PathConstraints(3, 3, 540, 720)))
+     autoChooser.getSelected()
     );
+//autoChooser.getSelected().andThen(AutoBuilder.pathfindThenFollowPath(path, new PathConstraints(3, 3, 540, 720)));
   }
 
 }
